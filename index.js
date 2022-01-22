20ca9dc30ff0bb68655338290c4515db9dcc6f7e
Darcy <darcy.lauder@shaw.ca>
2022-01-21 19:30:30 -0700

    added index and json

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (toN > fromN)
+    return toN + sum(fromN, toN - 1);
+  return fromN;
+
+};
 
 module.exports = sum;
